0d27aab4e16306dfae3e12958b57a433de546ae3
Aileen Padrón Torres <79520153+PinkBlure@users.noreply.github.com>
2023-09-28 20:19:12 +0100

    Add files via upload

---
+++ DAW_P1/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Main.
+ *
+ * @author Aileen Padrón Torres
+ * @since 27-09-2023
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        final String nombre = "Aileen";
+        final String ciclo = "DAW";
+        final String modulo = "programación";
+
+        System.out.println("Mi nombre es nombre " + nombre + ".");
+        System.out.println("Estudio el ciclo de " + ciclo + ".");
+        System.out.println("Este es el modulo de " + modulo + ".");
+
+    }
+
+}
